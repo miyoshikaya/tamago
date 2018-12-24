9d45db3c634aaec2fe12b7064328dc153713b05d
Malgorzata Kalek (malgkal848) <malgkal848@student.polsl.pl>
2018-12-24 23:30:59 +0100

    zedytowano funkcję doAddUser

--- ui/src/firebase/db.js
+++ ui/src/firebase/db.js
@@ -3,9 +3,63 @@ import { db } from './firebase';
 // User API
 export const doCreateUser = (id, email) => { 
 	db.ref(`users/${id}`).set({ 
-		"firebase-id": email, 
+		"id": email, 
+        "language": "korean",
+        "flashcards": [
+	        {
+	            "id": "0-0-0",
+	            "status": "learning"
+	        },
+	        {
+	        	"id": "0-0-1",
+	            "status": "learnt"
+	        },
+	        {
+	            "id": "0-0-2",
+	            "status": "mastered"
+	        }
+        ],
+        "pet-items": [
+            {
+                "id": "play",
+                "number": 3
+            },
+            {
+                "id": "food",
+                "number": 3
+            },
+            {
+                "id": "wash",
+                "number": 3
+            },
+            {
+                "id": "music",
+                "number": 3
+            }
+        ],
+        "timers": [
+            {
+                "id": "play",
+                "timestamp": "",
+                "timer": "12:0"
+            },
+            {
+                "id": "food",
+                "timestamp": "",
+                "timer": "12:45"
+            },
+            {
+                "id": "wash",
+                "timestamp": "",
+                "timer": "12:0"
+            },
+            {
+                "id": "music",
+                "timestamp": "",
+                "timer": "12:0"
+            }
+        ]
 	});
-	console.log("user id: " + id + "email: " + email);
 }
 
 export const onceGetUsers = () => 
