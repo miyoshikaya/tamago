2d2fce74d3be8f6bcdc1af3e622322933cd7d415
MacSpain <michkre163@student.polsl.pl>
2018-12-27 22:07:26 +0100

    poprawiono komunikat przy różnych hasłach

--- ui/src/components/HomePage/SettingSection/SettingsCard/PwdChange.js
+++ ui/src/components/HomePage/SettingSection/SettingsCard/PwdChange.js
@@ -2,84 +2,89 @@ import React, { Component } from 'react';
 import { auth } from '../../../../firebase';
 import './pwdchange.css';
 
-const byPropKey = (propertyName, value) => () => ({ 
-	[propertyName]: value, 
+const byPropKey = (propertyName, value) => () => ({
+	[propertyName]: value,
 });
 
 const INITIAL_STATE = { passwordOne: '', passwordTwo: '', error: null, };
 
-class PwdChangeForm extends Component { 
+class PwdChangeForm extends Component {
 	constructor(props) {
-	 super(props);
+		super(props);
 
-	this.state = { ...INITIAL_STATE };
-}
-
-onSubmit = (event) => { 
-	const { 
-		passwordOne,
-		passwordTwo 
-	} = this.state;
-
-	if(passwordOne === passwordTwo){
-
-		auth.doPasswordUpdate(passwordOne) 
-		.then(() => { 
-			this.setState({ ...INITIAL_STATE }); 
-		}) 
-		.catch(error => { 
-			this.setState(byPropKey('error', error)); 
-		});
-
-		event.preventDefault();
-		alert("Successfully managed to update the password.");
+		this.state = { ...INITIAL_STATE };
 	}
-	else if(passwordOne !== passwordTwo)
-		alert("Please provide identical passwords.");
-}
 
-render() {
-	const { passwordOne, 
-		passwordTwo, 
-		error, 
-	} = this.state;
+	onSubmit = (event) => {
 
-	const isInvalid = passwordOne !== passwordTwo || passwordOne === '';
+		const {
+			passwordOne,
+			passwordTwo
+		} = this.state;
 
-	return ( 
-		<form id="settings-form" onSubmit={this.onSubmit}> 
-			<div className="dropdownWrapper">
-              <div className="dropdown">
-                <div className="dropdownTitle">New password:</div>
-	                <div className="dropdownOption">
-	                 	<input 
-	                	value={passwordOne} 
-						onChange={event => this.setState(byPropKey('passwordOne', event.target.value))} 
-	                	type="password" 
-	                	placeholder="Insert new password" 
-	                	required 
-	                	className="pwd" />
-	            	</div>
-	            </div>
-	        </div>
-            <div className="dropdownWrapper">
-              <div className="dropdown">
-                <div className="dropdownTitle">Repeat the password:</div>
-	                <div className="dropdownOption">
-	                 	<input 
-	                	value={passwordTwo} 
-						onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))} 
-	                	type="password" 
-	                	placeholder="Insert new password" 
-	                	required 
-	                	className="pwd" />
-	            	</div>
-	            </div>
-	        </div>
-			<button className="btn-reset" disabled={isInvalid} type="submit"><span>Reset My Password</span></button>
-			{ error && <p>{error.message}</p> } 
-		</form> 
-	);	
+		console.log(event);
+		console.log(passwordTwo);
+		if (passwordOne === passwordTwo) {
+
+			auth.doPasswordUpdate(passwordOne)
+				.then(() => {
+					this.setState({ ...INITIAL_STATE });
+				})
+				.catch(error => {
+					this.setState(byPropKey('error', error));
+				});
+
+			event.preventDefault();
+			alert("Successfully managed to update the password.");
+		}
+		else {
+			event.preventDefault();
+			alert("Please provide identical passwords.");
+		}
+	}
+
+	render() {
+		const { passwordOne,
+			passwordTwo,
+			error,
+		} = this.state;
+
+		const isInvalid = passwordOne === '';
+
+		return (
+			<form id="settings-form" onSubmit={this.onSubmit}>
+				<div className="dropdownWrapper">
+					<div className="dropdown">
+						<div className="dropdownTitle">New password:</div>
+						<div className="dropdownOption">
+							<input
+								value={passwordOne}
+								onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+								type="password"
+								placeholder="Insert new password"
+								required
+								className="pwd" />
+						</div>
+					</div>
+				</div>
+				<div className="dropdownWrapper">
+					<div className="dropdown">
+						<div className="dropdownTitle">Repeat the password:</div>
+						<div className="dropdownOption">
+							<input
+								value={passwordTwo}
+								onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+								type="password"
+								placeholder="Insert new password"
+								required
+								className="pwd" />
+						</div>
+					</div>
+				</div>
+				<button className="btn-reset" disabled={isInvalid} type="submit"><span>Reset My Password</span></button>
+				{error && <p>{error.message}</p>}
+			</form>
+		);
 	}
 }
 export default PwdChangeForm;
